9947cdc6215242354893582f1566739744141494
nickkramer101 <nicholas.kramer@uvm.edu>
2025-07-29 13:42:45 -0400

    starting to get db in html

--- django_start/app/models.py
+++ django_start/app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# first step to getting database in html
+
+class InventoryItem(models.Model):
+    # add fields
+    name = models.CharField(max_length=100)
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    # Add other fields as needed from your inventory database
+
+    def __str__(self):
+        return self.name
