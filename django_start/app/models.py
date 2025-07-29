f1e8eede9dea537c2f3254f7e7d7e21604e1180f
nickkramer101 <nicholas.kramer@uvm.edu>
2025-07-29 17:21:06 -0400

    frontend not bugged rn

--- django_start/app/models.py
+++ django_start/app/models.py
@@ -3,16 +3,15 @@ from django.db import models
 # Create your models here.
 # first step to getting database in html
 
-class InventoryItem(models.Model):
+class Items(models.Model):
     # add fields
     item_id = models.IntegerField(primary_key=True)
-    name = models.CharField(max_length=200)
+    name = models.TextField()
     quantity = models.IntegerField()
     price = models.DecimalField(max_digits=10, decimal_places=2)
 
 
     class Meta:
-        managed = False  # Tell Django not to manage this table's creation
         db_table = 'items'  # The real table name in your DB
 
     def __str__(self):
--- django_start/app/views.py
+++ django_start/app/views.py
@@ -2,14 +2,12 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from .models import InventoryItem
+from .models import Items
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
 
 def template(request):
-    items = InventoryItem.objects.all()
-
-
+    items = Items.objects.all()
     return render(request, 'app/template.html', {'items': items})
