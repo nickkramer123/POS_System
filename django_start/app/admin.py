53f37110aef880d498192590308bab0d675fabc3
nickkramer101 <nicholas.kramer@uvm.edu>
2025-07-31 10:57:31 -0400

    search bar returns value

--- django_start/app/admin.py
+++ django_start/app/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Items
+from .models import Items, Transactions, TransactionItems
 
 
 # Register your models here.
 admin.site.register(Items)
+admin.site.register(Transactions)
+admin.site.register(TransactionItems)
+
--- django_start/app/views.py
+++ django_start/app/views.py
@@ -2,13 +2,24 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
-from .models import Items
+from .models import Items, Transactions, TransactionItems
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the xyz.")
 
 def template(request):
+    # get value from searchbar
+
+
+
     items = Items.objects.all()
-    print("Items exist?", bool(items))  # FALSE
-    return render(request, 'app/template.html', {'items': items})
+    transactions = Transactions.objects.all()
+    transactionsItems = TransactionItems.objects.all()
+    return render(request, 'app/template.html', {'items': items, 'transactions': transactions, 'transactionItems': transactionsItems})
+
+
+def getID(request):
+    query = request.GET.get('search_query', '')
+    print("user searched for: ", query)
+    return query
