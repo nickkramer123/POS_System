17f7d5ce40831fa3870eeab4634f7b9ba4848def
Cole Nam <colenam04@gmail.com>
2025-07-31 11:09:13 -0400

    More

--- django_start/app/models.py
+++ django_start/app/models.py
@@ -16,3 +16,30 @@ class Items(models.Model):
 
     def __str__(self):
         return self.name
+    
+class Transactions(models.Model):
+    # add fields
+    id = models.IntegerField(primary_key=True)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    timestamp = models.TextField()
+
+    class Meta:
+        db_table = 'transactions'  # The real table name in your DB
+
+    def __str__(self):
+        return self.name
+    
+
+
+class TransactionItems(models.Model):
+    # add fields
+    transaction_id = models.IntegerField()
+    item_id = models.IntegerField()
+    quantity = models.IntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    class Meta:
+        db_table = 'transaction_items'  # The real table name in your DB
+
+    def __str__(self):
+        return self.name
