b9f668ec7914092347f7595f4d6cf1abb92dda48
nickkramer101 <nicholas.kramer@uvm.edu>
2025-08-06 13:08:26 -0400

    fixed two bugs on github, including adding quantty

--- django_start/app/views.py
+++ django_start/app/views.py
@@ -31,24 +31,33 @@ def getID(request):
 def add_to_cart(request):
     
     item_id = getID(request)
-    
+    if not item_id: # for input validation
+        return redirect('home')
+
+
+
     item = find_item_by_id(item_id) #item object
+    if not item: # for input validation
+        return redirect('home')
 
 
     cart = request.session.get('cart', {})
     
     # Populate the cart in the session
+    
     if str(item_id) in cart:
         cart[str(item_id)]['quantity'] += 1
     else:
         cart[str(item_id)] = {
             'item_id': item_id, 
             'item' : item.name,
-            'quantity': 1, 
-            'price': item.price
+            'price': item.price,
+            'quantity': 1
         }
     request.session['cart'] = cart # This updates the session with the new cart data
     print("Item added to cart:", item_id)
+
+    
 
     # Prepare the cart data for the response
     # This is a list of dictionaries, each representing an item in the cart with its subtotal calculated
@@ -68,4 +77,6 @@ def add_to_cart(request):
 
 def clear_cart(request):
     request.session['cart'] = {}  # Clear the cart in the session
+    request.session['total'] = 0
+
     return redirect('home')  # Redirect to the template view after clearing the cart
